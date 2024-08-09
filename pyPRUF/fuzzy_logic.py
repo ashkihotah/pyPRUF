fbdf0807feeae38cc7c41b9ebd6da31b4a88b2f9
ashkihotah <nicoloresta02@gmail.com>
2024-08-09 18:32:09 +0200

    added docs in membership_functions.py

--- pyPRUF/fuzzy_logic.py
+++ pyPRUF/fuzzy_logic.py
@@ -11,8 +11,8 @@ class FuzzyOperator(Enum):
     A fuzzy operator is a function that takes memberships values
     and returns another membership value.
 
-    This class is an enumeration (`Enum`) that requires derived classes
-    to implement the `__call__` method, which should return a `float`.
+    This class is an enumeration (Enum) that requires derived classes
+    to implement the __call__ method, which should return a float.
     In this way, it is possible to provide for each fuzzy operator,
     several implementations (or functions) that will correspond
     to the values of the Enum.
@@ -31,14 +31,14 @@ class FuzzyOperator(Enum):
         specific behavior of the fuzzy operator.
 
         :returns: the result of the operation
-        :rtype: `float`
+        :rtype: float
         """
         pass
 
 class FuzzyUnaryOperator(FuzzyOperator):
     """
     An abstract base class that defines a generic fuzzy unary operator
-    by extending the `FuzzyOperator` Enum class.
+    by extending the FuzzyOperator Enum class.
     A fuzzy unary operator is a function that takes a
     membership value and returns another membership value.
     """
@@ -54,14 +54,14 @@ class FuzzyUnaryOperator(FuzzyOperator):
         :param float a: The membership value on which the
         operation is to be carried out.
         :returns: the result of the operation
-        :rtype: `float`
+        :rtype: float
         """
         pass
 
 class FuzzyBinaryOperator(FuzzyOperator):
     """
     An abstract base class that defines a generic fuzzy binary operator
-    by extending the `FuzzyOperator` Enum class.
+    by extending the FuzzyOperator Enum class.
     A fuzzy binary operator is a function that takes two
     memberships values and returns another membership value.
     """
@@ -77,7 +77,7 @@ class FuzzyBinaryOperator(FuzzyOperator):
         :param float a: The first membership value.
         :param float b: The second membership value.
         :returns: the result of the operation
-        :rtype: `float`
+        :rtype: float
         """
         pass
 
@@ -85,7 +85,7 @@ class FuzzyAnd(FuzzyBinaryOperator):
     """
     A class that defines the AND fuzzy binary operator
     and its truth functions by extending the
-    `FuzzyBinaryOperator` Enum class.
+    FuzzyBinaryOperator Enum class.
 
     Currently FuzzyAnd truth functions implemented are:
     1. MIN: The standard AND truth function.
@@ -104,8 +104,8 @@ class FuzzyAnd(FuzzyBinaryOperator):
 
         :param float a: The first membership value.
         :param float b: The second membership value.
-        :returns: `max(a + b - 1.0, 0.0)`.
-        :rtype: `float`
+        :returns: max(a + b - 1.0, 0.0).
+        :rtype: float
         """
         # return max(float(Decimal(a + b) - Decimal(1.0)), 0.0)
         return max(a + b - 1.0, 0.0)
@@ -118,8 +118,8 @@ class FuzzyAnd(FuzzyBinaryOperator):
 
         :param float a: The first membership value.
         :param float b: The second membership value.
-        :returns: `a * b`.
-        :rtype: `float`
+        :returns: a * b.
+        :rtype: float
         """
         # return float(Decimal(a) * Decimal(b))
         return a * b
@@ -132,9 +132,9 @@ class FuzzyAnd(FuzzyBinaryOperator):
 
         :param float a: The first membership value.
         :param float b: The second membership value.
-        :returns: `.0` if 1.0 != a and 1.0 != b
-        and `min(a, b)` otherwise.
-        :rtype: `float`
+        :returns: .0 if 1.0 != a and 1.0 != b
+        and min(a, b) otherwise.
+        :rtype: float
         """
         if 1.0 != a and 1.0 != b:
             return 0.0
@@ -142,15 +142,15 @@ class FuzzyAnd(FuzzyBinaryOperator):
 
     def __call__(self, a: float, b: float) -> float:
         """
-        Calculates the fuzzy AND of `a` and `b`.
+        Calculates the fuzzy AND of a and b.
 
         :param float a: The first membership value.
         :param float b: The second membership value.
         :returns: the result of the AND operation.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` and `b` are not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a and b are not of type
+        float in the interval [0, 1].
         """
         assert isinstance(a, float) and isinstance(b, float), "'a' and 'b' values must be floats!"
         assert 0.0 <= a and a <= 1.0 and 0.0 <= b and b <= 1.0, "'a' and 'b' must be between [0, 1]!"
@@ -160,14 +160,14 @@ class FuzzyOr(FuzzyBinaryOperator):
     """
     A class that defines the OR fuzzy binary operator
     and its truth functions by extending the
-    `FuzzyBinaryOperator` Enum class.
+    FuzzyBinaryOperator Enum class.
 
     Currently FuzzyAnd truth functions implemented are:
-    1. MAX: returns `max(a, b)`
-    2. LUKASIEWICZ: returns `min(a + b, 1.0)`.
-    3. ALGEBRAIC_SUM: returns `a + b - a * b`.
-    4. DRASTIC_SUM: returns `1.0` if .0 != a and .0 != b
-        and `max(a, b)` otherwise.
+    1. MAX: returns max(a, b)
+    2. LUKASIEWICZ: returns min(a + b, 1.0).
+    3. ALGEBRAIC_SUM: returns a + b - a * b.
+    4. DRASTIC_SUM: returns 1.0 if .0 != a and .0 != b
+        and max(a, b) otherwise.
     """
 
     MAX = max
@@ -180,8 +180,8 @@ class FuzzyOr(FuzzyBinaryOperator):
 
         :param float a: The first membership value.
         :param float b: The second membership value.
-        :returns: `min(a + b, 1.0)`.
-        :rtype: `float`
+        :returns: min(a + b, 1.0).
+        :rtype: float
         """
         return min(a + b, 1.0)
     
@@ -193,8 +193,8 @@ class FuzzyOr(FuzzyBinaryOperator):
 
         :param float a: The first membership value.
         :param float b: The second membership value.
-        :returns: `a + b - a * b`.
-        :rtype: `float`
+        :returns: a + b - a * b.
+        :rtype: float
         """
         # return float(Decimal(a + b) - Decimal(a) * Decimal(b))
         return a + b - a * b
@@ -207,9 +207,9 @@ class FuzzyOr(FuzzyBinaryOperator):
 
         :param float a: The first membership value.
         :param float b: The second membership value.
-        :returns: `1.0` if .0 != a and .0 != b
-        and `max(a, b)` otherwise.
-        :rtype: `float`
+        :returns: 1.0 if .0 != a and .0 != b
+        and max(a, b) otherwise.
+        :rtype: float
         """
         if .0 != a and .0 != b:
             return 1.0
@@ -217,15 +217,15 @@ class FuzzyOr(FuzzyBinaryOperator):
 
     def __call__(self, a: float, b: float) -> float:
         """
-        Calculates the fuzzy OR of `a` and `b`.
+        Calculates the fuzzy OR of a and b.
 
         :param float a: The first membership value.
         :param float b: The second membership value.
         :returns: the result of the OR operation.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` and `b` are not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a and b are not of type
+        float in the interval [0, 1].
         """
         assert isinstance(a, float) and isinstance(b, float), "'a' and 'b' values must be floats!"
         assert 0.0 <= a and a <= 1.0 and 0.0 <= b and b <= 1.0, "'a' and 'b' must be between [0, 1]!"
@@ -235,11 +235,11 @@ class FuzzyNot(FuzzyUnaryOperator):
     """
     A class that defines the NOT fuzzy binary operator
     and its truth functions by extending the
-    `FuzzyUnaryOperator` Enum class.
+    FuzzyUnaryOperator Enum class.
 
     Currently FuzzyAnd truth functions implemented are:
-    1. STANDARD: returns `1.0 - value`.
-    2. COSINE: returns `(1.0 + math.cos(math.pi * value)) / 2.0`.
+    1. STANDARD: returns 1.0 - value.
+    2. COSINE: returns (1.0 + math.cos(math.pi * value)) / 2.0.
     """
     
     @member
@@ -250,8 +250,8 @@ class FuzzyNot(FuzzyUnaryOperator):
 
         :param float a: The membership value on which the
         operation is to be carried out.
-        :returns: `1.0 - value`.
-        :rtype: `float`
+        :returns: 1.0 - value.
+        :rtype: float
         """
         # return float(Decimal(1.0) - Decimal(value))
         return 1.0 - value
@@ -264,23 +264,23 @@ class FuzzyNot(FuzzyUnaryOperator):
 
         :param float a: The membership value on which the
         operation is to be carried out.
-        :returns: `(1.0 + math.cos(math.pi * value)) / 2.0`.
-        :rtype: `float`
+        :returns: (1.0 + math.cos(math.pi * value)) / 2.0.
+        :rtype: float
         """
         # return (1.0 + math.cos(float(Decimal(math.pi) * Decimal(value)))) / 2.0
         return (1.0 + math.cos(math.pi * value)) / 2.0
 
     def __call__(self, a: float) -> float:
         """
-        Calculates the fuzzy NOT of `a`.
+        Calculates the fuzzy NOT of a.
 
         :param float a: The membership value on which the
         operation is to be carried out.
         :returns: the result of the NOT operation.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` is not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a is not of type
+        float in the interval [0, 1].
         """
         assert isinstance(a, float), "'a' value must be float!"
         assert 0.0 <= a and a <= 1.0, "'a' must be between [0, 1]!"
@@ -300,83 +300,83 @@ class FuzzyLogic():
     def and_fun(a: float, b: float) -> float:
         """
         Calls the currently FuzzyAnd truth function set with
-        `a` and `b` as parameters.
+        a and b as parameters.
 
         :param float a: The first membership value.
         :param float b: The second membership value.
         :returns: the result of the currently
         FuzzyAnd truth function set.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` and `b` are not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a and b are not of type
+        float in the interval [0, 1].
         """
         return FuzzyLogic.__and_fun(a, b)
 
     def or_fun(a: float, b: float) -> float:
         """
         Calls the currently FuzzyOr truth function set with
-        `a` and `b` as parameters.
+        a and b as parameters.
 
         :param float a: The first membership value.
         :param float b: The second membership value.
         :returns: the result of the currently
         FuzzyOr truth function set.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` and `b` are not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a and b are not of type
+        float in the interval [0, 1].
         """
         return FuzzyLogic.__or_fun(a, b)
 
     def not_fun(a: float) -> float:
         """
         Calls the currently FuzzyNot truth function set with
-        `a` as parameter.
+        a as parameter.
 
         :param float a: The membership value on which the
         operation is to be carried out.
         :returns: the result of the currently
         FuzzyNot truth function set.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` and `b` are not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a and b are not of type
+        float in the interval [0, 1].
         """
         return FuzzyLogic.__not_fun(a)
 
     def set_and_fun(and_fun: FuzzyAnd) -> None:
         """
-        Set `and_fun` as the current `FuzzyAnd` truth function.
+        Set and_fun as the current FuzzyAnd truth function.
 
-        :param float and_fun: The `FuzzyAnd` truth function.
+        :param float and_fun: The FuzzyAnd truth function.
 
-        :raises `AssertionError`: If `and_fun`
-        is not of type `FuzzyAnd`.
+        :raises AssertionError: If and_fun
+        is not of type FuzzyAnd.
         """
         assert isinstance(and_fun, FuzzyAnd), "'and_fun' must be of type 'FuzzyAnd'!"
         FuzzyLogic.__and_fun = and_fun
 
     def set_or_fun(or_fun: FuzzyOr) -> None:
         """
-        Set `or_fun` as the current `FuzzyOr` truth function.
+        Set or_fun as the current FuzzyOr truth function.
 
-        :param float or_fun: The `FuzzyOr` truth function.
+        :param float or_fun: The FuzzyOr truth function.
 
-        :raises `AssertionError`: If `or_fun`
-        is not of type `FuzzyOr`.
+        :raises AssertionError: If or_fun
+        is not of type FuzzyOr.
         """
         assert isinstance(or_fun, FuzzyOr), "'or_fun' must be of type 'FuzzyOr'!"
         FuzzyLogic.__or_fun = or_fun
         
     def set_not_fun(not_fun: FuzzyNot) -> None:
         """
-        Set `not_fun` as the current `FuzzyNot` truth function.
+        Set not_fun as the current FuzzyNot truth function.
 
-        :param float not_fun: The `FuzzyNot` truth function.
+        :param float not_fun: The FuzzyNot truth function.
 
-        :raises `AssertionError`: If `not_fun`
-        is not of type `FuzzyNot`.
+        :raises AssertionError: If not_fun
+        is not of type FuzzyNot.
         """
         assert isinstance(not_fun, FuzzyNot), "'not_fun' must be of type 'FuzzyNot'!"
         FuzzyLogic.__not_fun = not_fun
@@ -384,13 +384,13 @@ class FuzzyLogic():
 class LinguisticModifiers(FuzzyUnaryOperator):
     """
     A class that defines the linguistic modifiers
-    by extending the `FuzzyUnaryOperator` Enum class.
+    by extending the FuzzyUnaryOperator Enum class.
     A linguistic modifier is a function that takes
     a membership value and returns a modified
     membership value according to a linguistic
     semantic.
 
-    Currently `LinguisticModifiers` implemented are:
+    Currently LinguisticModifiers implemented are:
     1. VERY: returns the square power of the value in input.
     2. COSINE: returns the square root of the value in input.
     """
@@ -399,12 +399,12 @@ class LinguisticModifiers(FuzzyUnaryOperator):
     def VERY(value: float) -> float:
         """
         Calculates the 'very' linguistic modifier value
-        of the membership value `value`.
+        of the membership value value.
 
         :param float a: The first membership value on which the
         operation is to be carried out.
-        :returns: `value * value`
-        :rtype: `float`
+        :returns: value * value
+        :rtype: float
         """
         # return float(Decimal(value) * Decimal(value))
         return value * value
@@ -413,26 +413,26 @@ class LinguisticModifiers(FuzzyUnaryOperator):
     def MORE_OR_LESS(value: float) -> float:
         """
         Calculates the 'more or less' linguistic modifier value
-        of the membership value `value`.
+        of the membership value value.
 
         :param float a: The first membership value on which the
         operation is to be carried out.
-        :returns: `math.sqrt(value)`
-        :rtype: `float`
+        :returns: math.sqrt(value)
+        :rtype: float
         """
         return math.sqrt(value)
     
     def __call__(self, a: float) -> float:
         """
-        Calculates the linguistic modifier value of `a`.
+        Calculates the linguistic modifier value of a.
 
         :param float a: The membership value on which the
         operation is to be carried out.
         :returns: the result of the linguistic modifier applied.
-        :rtype: `float`
+        :rtype: float
 
-        :raises `AssertionError`: If `a` is not of type
-        `float` in the interval [0, 1].
+        :raises AssertionError: If a is not of type
+        float in the interval [0, 1].
         """
         assert isinstance(a, float), "'a' value must be float!"
         assert 0.0 <= a and a <= 1.0, "'a' must be between [0, 1]!"
--- pyPRUF/membership_functions.py
+++ pyPRUF/membership_functions.py
@@ -3,14 +3,46 @@ import math
 import sys
 
 class MembershipFunction(ABC):
+    """
+    Abstract base class for a membership function used
+    in fuzzy logic.
+    
+    Subclasses must implement the __call__
+    method, which defines the membership function
+    for a given element x.
+    """
 
     @abstractmethod
     def __call__(self, x) -> float:
+        """
+        Calculate the degree of membership for a given input x.
+        
+        :param x: The element for which to
+        calculate the membership degree.
+
+        :returns: The degree of membership for the
+        element x, in the range [0, 1].
+        :rtype: float
+        """
         pass
 
 class Triangular(MembershipFunction):
+    """
+    A triangular membership function for all those fuzzy sets
+    with domain the set of real numbers.
+    """
 
     def __init__(self, a: float, b: float, c: float) -> None:
+        """
+        Initialize the triangular membership function.
+
+        :param float a: The left endpoint of the triangle.
+        :param float b: The peak of the triangle.
+        :param float c: The right endpoint of the triangle.
+
+        :raises AssertionError: If the input values
+        do not satisfy the condition a < b < c.
+        """
         # super().__init__()
         assert isinstance(a, float), "'a' must be a float!"
         assert isinstance(b, float), "'b' must be a float!"
@@ -21,6 +53,19 @@ class Triangular(MembershipFunction):
         self.c = c
     
     def __call__(self, x) -> float:
+        """
+        Calculate the membership degree for a given
+        element x based on the triangular function.
+
+        :param float x: The element for which to
+        calculate the membership degree.
+        :returns:
+            - (x - self.a) / (self.b - self.a) if self.a <= x and x <= self.b \n
+            - (self.c - x) / (self.c - self.b) if self.b <= x and x <= self.c \n
+            - .0 otherwise \n
+        :rtype: float
+        :raises AssertionError: If x is not of type float.
+        """
         assert isinstance(x, float), "'x' must be a float!"
         if self.a <= x and x <= self.b:
             return (x - self.a) / (self.b - self.a)
@@ -29,8 +74,26 @@ class Triangular(MembershipFunction):
         return 0.0
     
 class Trapezoidal(MembershipFunction):
+    """
+    A trapezoidal membership function for all those fuzzy sets
+    with domain the set of real numbers.
+    """
 
     def __init__(self, a: float, b: float, c: float, d: float) -> None:
+        """
+        Initialize the trapezoidal membership function.
+
+        :param float a: The left endpoint of the trapezoid.
+        :param float b: The start of the top of the trapezoid.
+        :param float c: The end of the top of the trapezoid.
+        :param float d: The right endpoint of the trapezoid.
+
+        :raises AssertionError:
+            If the input values do not satisfy the conditions:\n
+            - a < b or (a == -sys.float_info.max and a == b)
+            - b < c
+            - c < d or (c == sys.float_info.max and c == d)
+        """
         # super().__init__()
         assert isinstance(a, float), "'a' must be a float!"
         assert isinstance(b, float), "'b' must be a float!"
@@ -45,6 +108,18 @@ class Trapezoidal(MembershipFunction):
         self.d = d
     
     def __call__(self, x) -> float:
+        """
+        Calculate the membership degree for a given element x based on the trapezoidal function.
+
+        :param float x: The element for which to calculate the membership degree.
+        :returns:
+            - 0 if x < a or x > d \n
+            - (x - a) / (b - a) if a <= x < b \n
+            - 1 if b <= x <= c \n
+            - (d - x) / (d - c) if c < x <= d \n
+        :rtype: float
+        :raises AssertionError: If x is not of type float.
+        """
         assert isinstance(x, float), "'x' must be a float!"
         if self.a <= x and x < self.b: # IMPORTANTE: il minore stretto a x < self.b evita che quando a = b = -Inf ci sia divisione per 0
             return (x - self.a) / (self.b - self.a)
@@ -55,8 +130,20 @@ class Trapezoidal(MembershipFunction):
         return 0.0
 
 class Bell(MembershipFunction):
+    """
+    A bell membership function for all those fuzzy sets
+    with domain the set of real numbers.
+    """
 
     def __init__(self, m: float, s: float) -> None:
+        """
+        Initialize the bell-shaped membership function.
+
+        :param float m: The center of the bell curve.
+        :param float s: The width of the bell curve.
+
+        :raises AssertionError: If the input values for m or s are not floats.
+        """
         # super().__init__()
         assert isinstance(m, float), "'a' must be a float!"
         assert isinstance(s, float), "'b' must be a float!"
@@ -64,5 +151,14 @@ class Bell(MembershipFunction):
         self.s = s
     
     def __call__(self, x) -> float:
+        """
+        Calculate the membership degree for a given
+        element x based on the bell-shaped function.
+
+        :param float x: The element for which to calculate the membership degree.
+        :returns: math.exp(-((x - self.m) ** 2) / (self.s ** 2))
+        :rtype: float
+        :raises AssertionError: If x is not of type float.
+        """
         assert isinstance(x, float), "'x' must be a float!"
         return math.exp(-((x - self.m) ** 2) / (self.s ** 2))
